fbdb9d8564d9813f09a8a0dba43381728dfb0ba5
Isobel <isobelmjones1@gmail.com>
2025-03-30 17:43:58 +1000

    solve_weighted_sokoban, testing, box tuple to (x,y,weight)

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -217,13 +217,84 @@ class SokobanPuzzle(search.Problem):
 
     
     def __init__(self, warehouse):
-        pass
+        self.warehouse = warehouse
+        
+        self.walls = set(warehouse.walls)
+        self.targets = set(warehouse.targets)
+        #self.weights = {box: weight for box, weight in zip(warehouse.boxes, warehouse.weights)}
+        
+        # each box represented as (x, y, weight) tuple
+        warehouse.boxes = [(box[0], box[1], weight) for box, weight in zip(warehouse.boxes, warehouse.weights)]
+        warehouse.boxes.sort(key=lambda b: (b[1], b[0]))
+        self.initial = (warehouse.worker, tuple(warehouse.boxes))
 
     def actions(self, state):
-        """
-        Return the list of actions that can be executed in the given state.
+        directions = ['Up', 'Down', 'Left', 'Right']
+        (wx, wy), boxes = state
+        boxes_xy = {(b[0], b[1]) for b in boxes} # extract (x,y) positions from boxes
+        actions = []
+        moves = {'Left': (-1, 0), 'Right': (1, 0), 'Up': (0, -1), 'Down': (0, 1)}
+        for action in directions:
+            dx, dy = moves[action]
+            nx, ny = wx + dx, wy + dy
+            if (nx, ny) in self.walls:
+                continue
+            if (nx, ny) in boxes_xy:
+                bnx, bny = nx + dx, ny + dy
+                if (bnx, bny) in boxes_xy or (bnx, bny) in self.walls:
+                    continue
+                actions.append(action)
+            else:
+                actions.append(action)
+        return actions
+    
+
+    def result(self, state, action):
+        (wx, wy), boxes = state
+        boxes = list(boxes)
+        dx, dy = {'Left': (-1, 0), 'Right': (1, 0), 'Up': (0, -1), 'Down': (0, 1)}[action]
+        new_worker = (wx + dx, wy + dy)
+        # if new_worker in boxes:
+        #     new_box = (new_worker[0] + dx, new_worker[1] + dy)
+        #     boxes.remove(new_worker)
+        #     boxes.append(new_box)
+
+        for i, (bx, by, w) in enumerate(boxes):
+            if (bx, by) == new_worker:
+                new_box = (bx + dx, by + dy, w)
+                boxes[i] = new_box
+                break
+        return (new_worker, tuple(sorted(boxes, key=lambda b: (b[1], b[0]))))
         
-        """
+    def goal_test(self, state):
+        _, boxes = state
+        return all((b[0], b[1]) in self.targets for b in boxes)
+
+    def path_cost(self, c, state1, action, state2):
+        _, b1 = state1
+        _, b2 = state2
+        # moved_box = set(b2) - set(b1)
+        # if moved_box:
+        #     box = moved_box.pop()
+        #     weight = self.weights.get(box, 0)
+        #     return c + 1 + weight
+        
+        moved_box = None
+        b1_xy = {(b[0], b[1]): b for b in b1}
+        b2_xy = {(b[0], b[1]): b for b in b2}
+        # identify  box that moved by comparing positions:
+        for pos, box in b2_xy.items():
+            if pos not in b1_xy:
+                moved_box = box
+                break
+        if moved_box:
+            return c + 1 + moved_box[2]
+        return c + 1
+  
+    def h(self, node):
+        _, boxes = node.state
+        return sum(min(abs(bx - tx) + abs(by - ty) for (tx, ty) in self.targets) for bx, by, _ in boxes)
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -320,6 +391,15 @@ def solve_weighted_sokoban(warehouse):
             C is the total cost of the action sequence C
 
     '''
+
+    problem = SokobanPuzzle(warehouse)
+    result = search.astar_graph_search(problem)
+    
+    if result is None:
+        return "Impossible", None
+    
+    return result.solution(), result.path_cost
+    
     
 
 
